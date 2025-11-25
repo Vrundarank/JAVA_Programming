c1011cb9651af9602463ffdabd03a660e9b37570
Vrundarank <vrudrankparite@gmail.com>
2025-11-25 23:53:31 +0530

    Added Application solutions for Assignment_31 with sample test case

---
+++ Assignment_31/Program31_4.java
@@ -0,0 +1,75 @@
+////////////////////////////////////////////////////////////////////////
+//
+//  Required Import Files
+//
+////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if(j == iCol || i == iRow || i == 1 || j ==1 || i == j)
+                {
+                    System.out.printf("*\t");
+                }
+                else
+                {
+                    System.out.printf(" \t");
+                }
+            }
+            System.out.printf("\n");
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function : Main
+//
+////////////////////////////////////////////////////////////////////////
+
+class Program31_4
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter the number of Rows: ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of Columns: ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+
+        pobj = null;
+        sobj.close();
+
+        System.gc();
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Test case handled by application
+//
+//  Input   :   iRow : 4    iCol : 4 
+//
+//  Output  :   *       *       *       *
+//              *       *               *
+//              *               *       *
+//              *       *       *       *
+//
+////////////////////////////////////////////////////////////////////////
